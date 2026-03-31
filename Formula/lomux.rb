6acf8613b242d002a55bca12d273a9acc34bcbb1
zblauser <54143933+zblauser@users.noreply.github.com>
2026-03-31 02:04:07 -0400

    lomux v1.1.0

--- Formula/lomux.rb
+++ Formula/lomux.rb
@@ -3,7 +3,7 @@ class Lomux < Formula
   homepage "https://github.com/zblauser/LoMux"
   url "https://github.com/zblauser/LoMux/archive/v1.1.0.tar.gz"
   sha256 "PLACEHOLDER_UPDATE_AFTER_RELEASE"
-  license "MIT"
+  license "GPL-3.0-only"
 
   depends_on "rust" => :build
   depends_on "ffmpeg"
